55322fb97cc7266fca10114a9ed80dfce6eb97d7
Clint Baker <clintjbaker@gmail.com>
2024-02-19 10:01:25 -0700

    adjust css files

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from 'react'
 import { UserContext } from '../../context/UserProvider'
 import RankCard from './RankCard'
 import './profile.css'
+// make sure capitalization is correct
 import '../css/rank.css'
 import '../css/ranks.css'
 
